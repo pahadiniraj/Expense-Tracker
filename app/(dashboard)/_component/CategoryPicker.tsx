2b72be1d69c7e58949ef884e50aa7406595fcde4
pahadiniraj <sharma12345niraj@gmail.com>
2024-11-12 15:31:25 +0545

    form build process

---
+++ app/(dashboard)/_component/CategoryPicker.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { TransactionType } from "@/lib/type";
+import React from "react";
+interface Props {
+  type: TransactionType;
+}
+
+const CategoryPicker = ({ type }: Props) => {
+  return <div>CategoryPicker</div>;
+};
+
+export default CategoryPicker;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,7 +72,7 @@ export const MobileNavbar = () => {
 export const DesktopNavbar = () => {
   return (
     <div className="hidden border-separate border-b bg-background md:block ">
-      <nav className="container flex items-center justify-between px-8">
+      <nav className="container flex items-center justify-between px-8 mx-auto">
         <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
           <Logo />
           <div className="flex ">
