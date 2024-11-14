5b82d556430003c2f0bc48f64730c9937f20587a
pahadiniraj <sharma12345niraj@gmail.com>
2024-11-14 09:54:07 +0545

    date picker added success

--- app/(dashboard)/_component/Overview.tsx
+++ app/(dashboard)/_component/Overview.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
 import { UserSettings } from "@prisma/client";
-import { startOfMonth } from "date-fns";
+import { differenceInDays, startOfMonth } from "date-fns";
 import React, { useState } from "react";
+import { toast } from "sonner";
 
 const Overview = ({ userSettings }: { userSettings: UserSettings }) => {
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
@@ -10,7 +13,33 @@ const Overview = ({ userSettings }: { userSettings: UserSettings }) => {
     to: new Date(),
   });
 
-  return <div>Overview</div>;
+  return (
+    <>
+      <div className="container flex flex-wrap items-center justify-between gap-2 py-6 px-8 mx-auto">
+        <h2 className="text-3xl font-bold ">Overview</h2>
+        <div className="flex items-center gap-3">
+          <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialDateTo={dateRange.to}
+            showCompare={false}
+            onUpdate={(values) => {
+              const { from, to } = values.range;
+
+              if (!from || !to) return;
+              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                toast.error(
+                  `The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`
+                );
+                return;
+              }
+
+              setDateRange({ from, to });
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Overview;
